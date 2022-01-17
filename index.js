6b934f6571a67ae368a565af20609eaa1121f97a
cesariote <44006333+cesariote@users.noreply.github.com>
2022-01-17 19:47:25 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayvalorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayvalorActual)
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
